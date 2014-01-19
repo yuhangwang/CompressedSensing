afd3eeafd80233abed3ec75adda39a87163ccd24
thelonious-monk <thelonious-monk@wp.pl>
2014-01-19 23:24:48 +0100

    Raport update
    *Intro added. LOTS of work!

--- CompressedSensing/src/algorithm/TvqcAlgorithm.cpp
+++ CompressedSensing/src/algorithm/TvqcAlgorithm.cpp
@@ -6,7 +6,6 @@ TvqcAlgorithm::TvqcAlgorithm() {}
 TvqcAlgorithm::~TvqcAlgorithm() {}
 
 cv::Mat TvqcAlgorithm::recoverImage(const cv::Mat& measurementMatrix, const cv::Mat& observations, const cv::Mat& startingSolution) {
-	cv::Mat outputImage = startingSolution.clone();
 
-	return outputImage;
+	return startingSolution;
 }
